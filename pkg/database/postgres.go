5be1b6419aa09e101be75677537d44af5ed95610
zulhanarif <zulhanarif77@gmail.com>
2026-02-20 12:21:05 +0900

    update: adjust the db url using the db already deployed in supabase

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -1,37 +1,31 @@
 package database
 
 import (
+	"log"
 	"authentication_api/internal/config"
 	"authentication_api/internal/model"
-	"fmt"
-	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable Timezone=Asia/Jakarta",
-		config.GetEnv("DB_HOST", "localhost"),
-		config.GetEnv("DB_USER", "postgres"),
-		config.GetEnv("DB_PASSWORD", ""),
-		config.GetEnv("DB_NAME", "mydb"),
-		config.GetEnv("DB_PORT", "5432"),
-	)
+	dsn := config.GetEnv("DB_URL", "host=localhost user=postgres password=rahasia dbname=mydb port=5432 sslmode=disable TimeZone=Asia/Jakarta")
 
+	// 2. Buka Koneksi dengan GORM
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Gagal koneksi ke database:", err)
 	}
 
-	log.Println("Database terhubung!")
+	log.Println("Database (Supabase) terhubung!")
 	return db
 }
 
 func MigrateDB(db *gorm.DB) {
 	err := db.AutoMigrate(&model.User{})
 	if err != nil {
-		log.Fatal("Gagal melakukan migrasi", err)
+		log.Fatal("Gagal melakukan migrasi:", err)
 	}
 	log.Println("Migrasi database berhasil!")
 }
